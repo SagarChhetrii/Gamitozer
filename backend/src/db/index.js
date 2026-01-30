218f0476cc993c3a10664636fa3f5e837549d0d6
Aparneet Dey <aparnitd@gmail.com>
2026-01-30 13:41:52 +0530

    Update backend dependencies and enhance app configuration. Added bcrypt, cookie-parser, cors, and jsonwebtoken to package.json and package-lock.json. Implemented CORS middleware and cookie parsing in app.js. Fixed MongoDB connection log in index.js.

--- backend/src/db/index.js
+++ backend/src/db/index.js
@@ -5,7 +5,7 @@ import { DB_NAME } from "../constants.js";
 export const ConnectDB = async () => {
     try {
         const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
-        console.log("MONGODB CONNECTION SUCCESSFULL :: CONNECTION HOST ::", connectionInstance.connection[0].host);
+        console.log("MONGODB CONNECTION SUCCESSFULL :: CONNECTION HOST ::", connectionInstance.connections[0].host);
     } catch (error) {
         console.log("MONGODB_CONNECTION_FAILED ::", error)
         process.exit(1)
