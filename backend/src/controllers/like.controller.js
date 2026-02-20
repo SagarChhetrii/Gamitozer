7496eda1bffdae5e8240713583e431b520070c31
Aparneet Dey <aparnitd@gmail.com>
2026-02-20 13:18:39 +0530

    Add blog like functionality to like controller and routes
    - Implemented toggleBlogLike method to allow users to like and unlike blogs
    - Added validation for blog ID and user authentication
    - Updated like routes to include new endpoint for toggling blog likes
    - Refactored error messages for clarity

--- backend/src/controllers/like.controller.js
+++ backend/src/controllers/like.controller.js
@@ -1,12 +1,12 @@
 import { Like } from "../models/like.model.js";
 import { ApiError } from "../utils/ApiError.js";
-import { ApiResponse } from "../utils/ApiResponse";
+import { ApiResponse } from "../utils/ApiResponse.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
 
 const toggleGameLike = asyncHandler( async (req, res) => {
     const {gameId} = req.params;
 
-    if(!gameId?.trim()) throw new ApiError(400, "Game is is required");
+    if(!gameId?.trim()) throw new ApiError(400, "Game id is required");
 
     let likedGame = await Like.findOne({
         game: gameId,
@@ -38,3 +38,45 @@ const toggleGameLike = asyncHandler( async (req, res) => {
         )
     )
 })
+
+const toggleBlogLike = asyncHandler( async (req, res) => {
+    const {blogId} = req.params;
+
+    if(!blogId?.trim()) throw new ApiError(400, "Blog id is required");
+
+    let likedBlog = await Like.findOne({
+        blog: blogId,
+        likedBy: req.user?._id
+    });
+
+    if(likedBlog) {
+        const unLikeResponse = await Like.deleteOne({
+            blog: blogId,
+            likedBy: req.user?._id
+        })
+
+        if(!unLikeResponse.acknowledged) throw new ApiError(500, "Something went wrong while toggling like");
+        likedBlog = null;
+    } else {
+        likedBlog =  await Like.create({
+            blog: blogId,
+            likedBy: req.user?._id
+        })
+    }
+
+    res
+    .status(200)
+    .json(
+        new ApiResponse(
+            201,
+            likedBlog,
+            "Game like toggled successfully"
+        )
+    )
+})
+
+
+export {
+    toggleGameLike,
+    toggleBlogLike
+}
--- backend/src/routes/like.route.js
+++ backend/src/routes/like.route.js
@@ -1,7 +1,14 @@
 import {Router} from "express";
+import { verifyToken } from "../middlewares/auth.middleware.js";
+import { toggleBlogLike, toggleGameLike } from "../controllers/like.controller.js";
 
 
 const router = Router();
 
+router.use(verifyToken)
+
+router.route("/game-toggle/:gameId").patch(toggleGameLike);
+
+router.route("/blog-toggle/:blogId").patch(toggleBlogLike);
 
 export default router;
