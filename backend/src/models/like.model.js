c7756ab147340caef682e78612feaa6c7bf46b99
Aparneet Dey <aparnitd@gmail.com>
2026-02-20 10:55:19 +0530

    Add Like model for game, blog, and comment interactions
    - Created a new Like model with references to Game, Blog, Comment, and User
    - Included timestamps for tracking like creation and updates

---
+++ backend/src/models/like.model.js
@@ -0,0 +1,28 @@
+import mongoose, { Schema } from "mongoose";
+
+
+const likeSchema = new Schema(
+    {
+        game: {
+            type: Schema.Types.ObjectId,
+            ref: "Game"
+        },
+        blog: {
+            type: Schema.Types.ObjectId,
+            ref: "Blog"
+        },
+        comment: {
+            type: Schema.Types.ObjectId,
+            ref: "Comment"
+        },
+        likedBy: {
+            type: Schema.Types.ObjectId,
+            ref: "User"
+        },
+    },
+    {
+        timestamps: true
+    }
+)
+
+export const Like = mongoose.model("Like", likeSchema)
