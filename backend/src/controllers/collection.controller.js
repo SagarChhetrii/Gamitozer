47f5e72218ef1a09deaabd00aa25edc073b99627
Aparneet Dey <aparnitd@gmail.com>
2026-03-02 00:30:51 +0530

    Add getUserCollections functionality to collection controller and routes
    - Introduced getUserCollections controller to fetch collections for a specific user with validation
    - Implemented aggregation pipeline to filter collections based on ownership and visibility
    - Updated collection routes to include a new endpoint for retrieving user collections

--- backend/src/controllers/collection.controller.js
+++ backend/src/controllers/collection.controller.js
@@ -2,10 +2,45 @@ import { asyncHandler } from "../utils/asyncHandler.js";
 import { ApiError } from "../utils/ApiError.js";
 import { Collection } from "../models/collection.model.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
+import { match } from "node:assert";
+import mongoose from "mongoose";
 
 const getUserCollections = asyncHandler( async (req, res) => {
     const { userId } = req.params;
     
+    if(!userId?.trim()) throw new ApiError(400, "User id is required");
+
+    if(!mongoose.Types.ObjectId.isValid(userId)) throw new ApiError(400, "Invalid user id");
+
+    const pipeline = [
+        {
+            $match: {
+                owner: new mongoose.Types.ObjectId(userId),
+            }
+        }
+    ]
+
+    if(userId !== req.user?._id.toString()) {
+        pipeline.push(
+            {
+                $match: {
+                    visibility: "public"
+                }
+            }
+        )
+    }
+
+    const collections = await Collection.aggregate(pipeline);
+
+    res
+    .status(200)
+    .json(
+        new ApiResponse(
+            200,
+            collections,
+            "Collections fetched successfully"
+        )
+    )
 })
 
 const publishACollection = asyncHandler( async (req, res) => {
@@ -35,5 +70,6 @@ const publishACollection = asyncHandler( async (req, res) => {
 })
 
 export {
+    getUserCollections,
     publishACollection
 }
--- backend/src/routes/collection.route.js
+++ backend/src/routes/collection.route.js
@@ -1,10 +1,12 @@
 import { Router } from "express";
 import { verifyToken } from "../middlewares/auth.middleware.js";
-import { publishACollection } from "../controllers/collection.controller.js";
+import { getUserCollections, publishACollection } from "../controllers/collection.controller.js";
 
 const router = Router();
 
 router.route("/")
 .post(verifyToken, publishACollection);
+
+router.route("/:userId").get(getUserCollections);
 
 export default router;
